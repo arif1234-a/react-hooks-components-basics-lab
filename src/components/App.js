ff717e7c7602c0fc14f9690a9cb35942978e6f5b
arifrahim <arifrahimjamal@gmail.com>
2025-04-25 07:02:32 +0300

    dene

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,25 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+{
+  /* write an <About> component here */
+}
+function About() {
+  return (
+    <div id="About">
+      <h2>
+        <a href="#about">About</a>
+      </h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
